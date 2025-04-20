a2c1f481fe2700b599b990fcfa0390f810220f6a
Slayder <slayderreyescuellar@gmail.com>
2025-04-19 23:01:31 -0500

    the font size at button was change

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    js: './src/app.js',
+    css: './src/sass/main.scss'
+  },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'main.js',
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader' // Compilador de sass para el archvo css
+        ],
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'theme.css',
+    }),
+  ],
   mode: 'development',
 };
